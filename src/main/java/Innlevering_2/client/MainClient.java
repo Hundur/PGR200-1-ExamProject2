115bd643a94dc09b8b6bfb53ca10d50d3342990d
Jesper Dahl Elllingsen <jesper.dahl.ellingsen@hotmail.com>
2017-11-13 12:54:29 +0100

    Client now connects to localhost, instead of ipv4 adress

--- src/main/java/Innlevering_2/client/MainClient.java
+++ src/main/java/Innlevering_2/client/MainClient.java
@@ -1,13 +1,10 @@
 package Innlevering_2.client;
 
-import Innlevering_2.client.io.IO_Controller;
-
 public class MainClient
 {
     public static void main(String [] args)
     {
-        //Todo: Find IPv4 address dynamically
-        Client client = new Client("192.168.38.103", 3000);
+        Client client = new Client("localhost", 3000);
 
         if(client != null)
         {
