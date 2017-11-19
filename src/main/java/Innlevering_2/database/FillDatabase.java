5aa8a68d318057cd0313b694f010025e861c616a
Jesper Dahl Elllingsen <jesper.dahl.ellingsen@hotmail.com>
2017-11-19 21:18:39 +0100

    Stuff finally works

--- src/main/java/Innlevering_2/database/FillDatabase.java
+++ src/main/java/Innlevering_2/database/FillDatabase.java
@@ -14,9 +14,7 @@ public class FillDatabase
         IO_Controller io = new IO_Controller("db.properties");
 
         io.createTables();
-        io.insertIntoTable(io.split(io.readInput("C:\\Users\\Bruker\\" +
-                                                         "IdeaProjects\\Innlevering_2\\" +
-                                                         "src\\main\\" +
+        io.insertIntoTable(io.split(io.readInput("src\\main\\" +
                                                          "resources\\DBTestInput")));
     }
 }
